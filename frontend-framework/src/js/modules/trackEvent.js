897bbdee7ab5af16cad3cdb4e8a57aa5f028545b
Florian Reisinger <reisi007@gmail.com>
2020-10-17 10:38:31 +0200

    More user tracking

--- frontend-framework/src/js/modules/trackEvent.js
+++ frontend-framework/src/js/modules/trackEvent.js
@@ -2,7 +2,6 @@ define('trackAction', [], function () {
     const matomo = _paq;
 
     return function (input) {
-        console.log("Tracking" + JSON.stringify(input))
         if (typeof input.value === 'undefined')
             matomo.push([
                 'trackEvent',
