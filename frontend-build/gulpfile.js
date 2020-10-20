44895ee618d83b7d987e8498bb620251e687134f
Florian Reisinger <reisi007@gmail.com>
2020-10-20 20:57:34 +0200

    Improved build (Part 2)

---
+++ frontend-build/gulpfile.js
@@ -0,0 +1,85 @@
+// fetch command line arguments
+const arg = (argList => {
+    let arg = {}, a, opt, thisOpt, curOpt;
+    for (a = 0; a < argList.length; a++) {
+        thisOpt = argList[a].trim();
+        opt = thisOpt.replace(/^-+/, '');
+
+        if (opt === thisOpt) {
+            // argument value
+            if (curOpt) arg[curOpt] = opt;
+            curOpt = null;
+        } else {
+            // argument name
+            curOpt = opt;
+            arg[curOpt] = true;
+        }
+    }
+    return arg;
+})(process.argv);
+
+const
+    gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    $ = require('gulp-load-plugins')({lazy: true}),
+    serveStatic = require('serve-static'),
+    base = './../' + arg.target + '/',
+    frameworkStatic = './../frontend-framework/out/**/*',
+    frameworkJsCss = './../frontend-framework/generated/**/*';
+
+gulp.task('copyStatic', function () {
+    return gulp
+        .src(base + 'src/static/**/*', {dot: true})
+        .pipe(gulp.dest(base + 'generated'))
+});
+
+gulp.task('copyFrameworkStatic', function () {
+    return gulp
+        .src(frameworkStatic, {dot: true})
+        .pipe(gulp.dest(base + 'generated'))
+});
+
+gulp.task('copyFrameworkJsCss', function () {
+    return gulp
+        .src(frameworkJsCss, {dot: true})
+        .pipe(gulp.dest(base + 'generated'))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            middleware: [
+                serveStatic(base + "generated/")
+            ],
+            injectChanges: true
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    // Watch framework
+    gulp.watch(base + "generated/**/*.html").on('change', browserSync.reload);
+    // Watch static files
+    gulp.watch(frameworkStatic, ['copyFrameworkStatic', browserSync.reload]);
+    // Watch .js / .css  files
+    gulp.watch(frameworkJsCss, ['copyFrameworkJsCss', browserSync.reload]);
+    // Watch static files
+    gulp.watch(base + 'src/static/**/*.*', ['copyStatic', browserSync.reload]);
+});
+
+gulp.task('default', function () {
+    gulp.start(
+        'copyStatic',
+        'copyFrameworkStatic',
+        'copyFrameworkJsCss',
+        'browser-sync',
+        'watch'
+    );
+});
+
+gulp.task('release', function () {
+    gulp.start(
+        'copyStatic',
+        'copyFrameworkStatic'
+    );
+});
