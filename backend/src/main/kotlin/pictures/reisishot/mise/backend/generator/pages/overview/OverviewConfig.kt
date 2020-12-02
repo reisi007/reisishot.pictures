b945850a79df7eb3590215d2b46850031ecdb8be
Florian Reisinger <reisi007@gmail.com>
2020-12-02 20:02:39 +0100

    Formatting

--- backend/src/main/kotlin/pictures/reisishot/mise/backend/generator/pages/overview/OverviewConfig.kt
+++ backend/src/main/kotlin/pictures/reisishot/mise/backend/generator/pages/overview/OverviewConfig.kt
@@ -1,5 +1,8 @@
 package pictures.reisishot.mise.backend.generator.pages.overview
 
-data class OverviewConfig(val name: String, val _style: String?)
+data class OverviewConfig(
+        val name: String,
+        val _style: String?
+)
 
 fun OverviewConfig?.computeStyle() = this?._style ?: "default"
