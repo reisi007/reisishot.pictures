d0e8c6b1f09c5a298158244b17cacb8aaedd971c
Florian Reisinger <reisi007@gmail.com>
2022-01-28 20:25:59 +0100

    Included code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,21 @@ repositories {
     mavenCentral()
 }
 
+sonarqube {
+    properties {
+        property("sonar.projectKey", "reisi007_reisishot.pictures")
+        property("sonar.organization", "reisi007")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property(
+            "sonar.exclusions",
+            listOf(
+                "**/backend/html/src/main/java/**/*" // (Once) generated / copied code
+            )
+        )
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.buildDir}/reports/jacoco.xml")
+    }
+}
+
 
 subprojects {
 
@@ -20,8 +35,6 @@ subprojects {
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
-    apply(plugin = "jacoco")
-    apply(plugin = "org.sonarqube")
     apply(plugin = "jacoco")
 
     jacoco {
@@ -75,21 +88,6 @@ subprojects {
         testImplementation("org.assertj:assertj-core:${Dependencies.ASSERTJ_VERSION}")
         testImplementation("org.junit.jupiter:junit-jupiter-api:${Dependencies.JUNIT_VERSION}")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Dependencies.JUNIT_VERSION}")
-    }
-
-    sonarqube {
-        properties {
-            property("sonar.projectKey", "reisi007_reisishot.pictures")
-            property("sonar.organization", "reisi007")
-            property("sonar.host.url", "https://sonarcloud.io")
-            property(
-                "sonar.exclusions",
-                listOf(
-                    "**/backend/html/src/main/java/**/*" // (Once) generated / copied code
-                )
-            )
-            property("sonar.coverage.jacoco.xmlReportPaths", "${project.buildDir}/reports/jacoco.xml")
-        }
     }
 
     tasks.jacocoTestReport {
